0fd4ad16f327316531ee65e0d4d05eb570a311de
snopzyz <78973908+Chivas-Regal@users.noreply.github.com>
2021-10-06 02:38:11 +0800

    f

--- tools/test.sh
+++ tools/test.sh
@@ -1,12 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Using HTML-proofer to test site.
 #
 # Requirement: https://github.com/gjtorikian/html-proofer
 #
 # Usage: bash /path/to/test.sh [indicated path]
+#
+# v2.0
+# https://github.com/cotes2020/jekyll-theme-chirpy
+# © 2020 Cotes Chung
+# MIT Licensed
 
 DEST=_site
+URL_IGNORE=cdn.jsdelivr.net
 
 _build=false
 
@@ -61,4 +67,6 @@ fi
 bundle exec htmlproofer "$DEST" \
   --disable-external \
   --check-html \
-  --allow_hash_href
+  --empty_alt_ignore \
+  --allow_hash_href \
+  --url_ignore $URL_IGNORE
